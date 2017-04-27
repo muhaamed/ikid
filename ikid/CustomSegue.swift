fe2ff7df685818fbdad400e29ace9a02a0d232cc
muhaamed <mold81193@gmail.com>
2017-04-26 23:41:54 -0700

    Deployment target to ios8.4

--- ikid/CustomSegue.swift
+++ ikid/CustomSegue.swift
@@ -18,24 +18,11 @@ class CustomSegue: UIStoryboardSegue {
         
         
         let window = UIApplication.shared.delegate?.window!
-        //set the destination View center
-        //destinationView?.center = CGPoint(x: (sourceView?.center.x)!, y: (sourceView?.center.y)! - (destinationView?.center.y)!)
         
-        // the Views must be in the Window hierarchy, so insert as a subview the destionation above the source
         window?.insertSubview(destinationView!, aboveSubview: sourceView!)
         
         //create UIAnimation- change the views's position when present it
         UIView.transition(with: window!, duration: 0.8, options: .transitionFlipFromLeft, animations: nil, completion: nil)
-        //        UIView.animate(withDuration: 0.4,
-        //                       animations: {
-        //                        destinationView?.center = CGPoint(x: (sourceView?.center.x)!, y: (sourceView?.center.y)!)
-        //                        sourceView?.center = CGPoint(x: (sourceView?.center.x)!, y: 0 + 2 * (destinationView?.center.y)!)
-        //        }, completion: {
-        //            (value: Bool) in
-        //            self.source.navigationController?.pushViewController(self.destination, animated: false)
-        //            
-        //            
-        //        })
-    }
+            }
 
 }
